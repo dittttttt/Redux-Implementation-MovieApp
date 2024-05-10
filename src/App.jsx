6afbda29b5f5a4d1867bc1204019ce25a2b56386
dittttttt <adityahputra2153@gmail.com>
2024-05-10 21:30:33 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  Router,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 import Home from "./dashboard.jsx";
 import TopRated from "./topRated/topRated.jsx";
@@ -11,19 +17,20 @@ import Register from "./register.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/login", element: <Login /> },
-    { path: "/register", element: <Register /> },
-    { path: "/top-rated", element: <TopRated /> },
-    { path: "/detail", element: <Detail /> },
-    { path: "/popular", element: <Popular /> },
-    { path: "/upcoming", element: <Upcoming /> },
-    { path: "/resultSearch", element: <ResultSearch /> },
-  ]);
   return (
     <GoogleOAuthProvider clientId="872362668839-cacmn49kvqddbb69mu04ig4l1lmpa6ao.apps.googleusercontent.com">
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/top-rated" element={<TopRated />}></Route>
+          <Route path="/detail" element={<Detail />}></Route>
+          <Route path="/popular" element={<Popular />}></Route>
+          <Route path="/upcoming" element={<Upcoming />}></Route>
+          <Route path="/resultSearch" element={<ResultSearch />}></Route>
+        </Routes>
+      </Router>
     </GoogleOAuthProvider>
   );
 }
